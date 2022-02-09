ec07819f3655f18413fe08517423bc1716f1935f
l0o0tus <l0o0tus@naver.com>
2022-02-09 23:44:56 +0900

    commit

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-const loginForm = document.getElementById("login-form");
-const logginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+const logginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+
+
+function onLoginBtnClick() {
+    console.log("hello", logginInput.value);
+}
+
+
+loginButton.addEventListener("click", onLoginBtnClick)
