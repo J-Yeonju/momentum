18829349bf6229cb8266f42182822ab3bd9c2797
l0o0tus <l0o0tus@naver.com>
2022-02-10 23:03:14 +0900

    name length

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-const logginInput = document.querySelector("#login-form input");
+const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 
 function onLoginBtnClick() {
-    console.log("hello", logginInput.value);
+    const username = loginInput.value;
+    if (username === "") {
+        alert("Please write your name");
+    } else if(username.length > 15) {
+        alert("Your name is too long.")
+    }
 }
 
 
