a4b333cbfcc323472989b4adba25891b714cc144
l0o0tus <l0o0tus@naver.com>
2022-02-21 23:45:32 +0900

    loading username

--- app.js
+++ app.js
@@ -10,16 +10,13 @@ function onLoginSubmit(event) {
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
     localStorage.setItem(USERNAME_KEY, username)
+    paintGreetings(username);
+}
+
+function paintGreetings(username){
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
-
-function paintGreetings(){
-    greeting.innerText = `Hello ${savedUsername}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
-}
-
-
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
@@ -27,6 +24,5 @@ if (savedUsername === null) {
     loginForm.classList.remove(HIDDEN_CLASSNAME);
     loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    paintGreetings(savedUsername);
 }
