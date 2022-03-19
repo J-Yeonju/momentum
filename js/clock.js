e367610d3238e63fa163e868eb09c0130a5c47c6
l0o0tus <l0o0tus@naver.com>
2022-03-19 19:57:20 +0900

    clock end

--- js/clock.js
+++ js/clock.js
@@ -1,11 +1,11 @@
 const clock = document.querySelector("h2#clock");
 
 function getClock() {
-    const data = new Date();
-    const hours = String(date.getHours()).paddStart(2, "0");
+    const date= new Date();
+    const hours = String(date.getHours()).padStart(2, "0");
     const minutes = String(date.getMinutes()).padStart(2, "0");
     const seconds = String(date.getSeconds()).padStart(2, "0");
-    clock.innerTEXT = '${hours}:${minutes};${seconds}';
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 getClock();
