df51a126a5d16dc65e94b336a992621461303669
l0o0tus <l0o0tus@naver.com>
2022-03-30 22:14:41 +0900

    Wearther2

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,17 @@
+const { response } = require("express");
+
 function onGeoOk(position) {
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You livevv in", lat, lng);
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            city.innerText = data.name;
+            weather.innerText = '${data.weather[0].main} / ${data.main.temp}';
+        })
 }
+
 function onGeoError() {
     alert("can't find you. No weather for you.");
 }
